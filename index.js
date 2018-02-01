2a65b16975aa82ed13ba8e217c16eb956e74991d
Ashwin George Philip <ashwinphilip27@gmail.com>
2018-02-01 03:08:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// write your code below!
+function happyHolidays(){
+  var greeting = "Happy holidays!"
+  return greeting
+}
+
+function happyHolidaysTo(name){
+  var greeting = "Happy holidays, you!"
+  return greeting
+}
+
+function happyHolidayTo(holiday, name){
+  var greeting = "Happy Independence Day, you!"
+  return greeting
+}
+
+function holidayCountdown(holiday, days){
+  var greeting = "test"
+  return greeting
+}
